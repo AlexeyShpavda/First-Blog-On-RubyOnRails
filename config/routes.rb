3840fe7d7f82e9f51b941ec51a83701de7e3d99b
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-01-18 00:22:32 +0300

    Removal of excess

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'posts#index'
+
   resources :posts, only: [:show, :index]
   resources :tags, only: [:show]
   resources :categories, only: [:show]
